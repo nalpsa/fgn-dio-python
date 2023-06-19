6bc6a154e6e120b96770bed8d3e405d65a476129
nalpsa <fabiodegoesnunes@gmail.com>
2023-06-19 15:31:04 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,5 @@
+# FGN - Formação Python Developer
+
+<a name="projeto"></a>
+
+- [Projeto 1 - Sistema Bancario](https://github.com/nalpsa/
---
+++ Sistema-Bancario/Sistema Bancario.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    
+    opcao = input(menu)
+    
+    if opcao == "d":  
+        
+        valor = float(input("Informe o valor do depósito: "))
+        
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação inválida, somente valores positivos são permitidos!")
+        
+    elif opcao == "s": 
+        """
+            o sistema deve permitir realizar 3 saqques diarios com o limite maximo de R$ 500,00 por saque
+            o sistema deve gerar uma mensagem informando que nao tem limite disponivel            
+        """
+        valor = float(input("Informe o valor do saque: "))
+        
+        excedeu_saldo = valor > saldo
+        
+        excedeu_limite = valor > limite
+        
+        excedeu_saques = numero_saques >= LIMITE_SAQUES       
+        
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+       
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque excede o limite.")        
+        
+        elif excedeu_saques:
+            print("Operação falhou! Numero máximo de saques excedido.")
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+            
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+        
+    elif opcao == "e":   
+        """
+            esta opção deve listar todos os saques e depositos e no final informar o saldo atual da conta
+            valores devem ser exibidos no formato: R$ 1500.45
+        """
+        print("\n==================== EXTRATO ====================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("\n=================================================")
+    elif opcao == "q":
+        break
+    else:
+        print("Operação inválida, por favor selecione novamente a opção desejada.")
+
